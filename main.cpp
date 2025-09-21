fdd0bfdeeeff7133671c7727a57972d7c43d990a
Warioisreal <ershovda12@gmail.com>
2025-09-21 20:17:40 +0300

    remove test_array files

--- main.cpp
+++ main.cpp
@@ -61,20 +61,7 @@ int main(void) {
 /*
 -----------------testing different types of arrays------------------------
               ----------**trash coding**-------------
-    const size_t strf_count = 10; // [367]
-    const size_t strf_size = 14;
-    const ssize_t line_size = 60;
 
-    char rect_text[strf_size * strf_count * line_size] = {'\0'};
-    char*** ptr_text = (char***)calloc(strf_size, sizeof(char**));
-
-    CreateRectArr(rect_text, line_size, strf_count, strf_size, filename);
-    PrintRectArr(rect_text, line_size, strf_count, strf_size);
-    //SortRectArr(text, line_size, strf_count, strf_size);
-    CreatePtrArr(ptr_text, strf_count, strf_size, filename);
-    PrintPtrArr(ptr_text, strf_count, strf_size);
-    //SortPtrArr(text, line_size, strf_count, strf_size);
-    ClearMemArr(ptr_text, strf_count, strf_size);
 --------------------------------------------------------------------------
 */
     return 0;
